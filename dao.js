f32cd9b050b1f6ad74297144e8f109fd809084b5
Jamie Dishy <jdishy@jdishy-mac.local>
2022-02-22 00:06:47 -0500

    Updated readme for my local

--- dao.js
+++ dao.js
@@ -1,76 +1,93 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const con = mysql.createConnection({
-    host:               'localhost',
-    user:               'root',
-    password:           '',
-    database:           'project4481-2.0'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "project4481-2.0",
 });
 
-
 module.exports = {
+  //  Insert client id to its table (client)
+  getClientId(id, success, failure = console.log) {
+    con.query("INSERT INTO client SET client_id = ?", id, (err, rows) => {
+      if (err == null) {
+        console.log(`${id} has been added to client table`);
+      } else {
+        failure(err);
+      }
+    });
+  },
 
-    //  Insert client id to its table (client)
-    getClientId(id, success, failure = console.log) {
-        con.query('INSERT INTO client SET client_id = ?', id, (err, rows) => {
-            if(err == null) {
-                console.log(`${id} has been added to client table`);
-            } else {
-                failure(err);
-            }
-        });
-    },
+  //  Get user and password for authentication --> return name and provider ID
+  getProviderByEmail(queries, success, failure = console.log) {
+    con.query(
+      "SELECT name, provider_id FROM provider WHERE email = ? AND password = ?",
+      [queries.email, queries.password],
+      (err, rows) => {
+        if (err == null) {
+          success(rows[0]);
+        } else {
+          failure(err);
+        }
+      }
+    );
+  },
 
-    //  Get user and password for authentication --> return name and provider ID
-    getProviderByEmail(queries, success, failure = console.log) {
-        con.query('SELECT name, provider_id FROM provider WHERE email = ? AND password = ?', [queries.email, queries.password], (err, rows) => {   
-            if(err == null) {
-                success(rows[0]);
-            } else {
-                failure(err);
-            }
-        });
-    },
+  //  get conversation history
+  getConversationHistory(queries, success, failure = console.log) {
+    con.query(
+      "SELECT * FROM message WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)",
+      [queries.sender, queries.receiver, queries.receiver, queries.sender],
+      (err, rows) => {
+        if (err == null) {
+          success(rows);
+        } else {
+          failure(err);
+        }
+      }
+    );
+  },
 
-    //  get conversation history
-    getConversationHistory(queries, success, failure = console.log) {
-        con.query('SELECT * FROM message WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)', [queries.sender, queries.receiver, queries.receiver, queries.sender], (err, rows) => {
-            if(err == null) {
-                success(rows);
-            } else {
-                failure(err);
-            }
-        });
-    },
+  postConversation(queries, success, failure = console.log) {
+    con.query(
+      "INSERT INTO message (sender_id, receiver_id, content) VALUES (?, ?, ?)",
+      [queries.sender, queries.receiver, queries.content],
+      (err, rows) => {
+        if (err == null) {
+          success(console.log(`${queries.content} added to table`));
+        } else {
+          failure(err);
+        }
+      }
+    );
+  },
 
-    postConversation(queries, success, failure = console.log) {
-        con.query('INSERT INTO message (sender_id, receiver_id, content) VALUES (?, ?, ?)', [queries.sender, queries.receiver, queries.content], (err, rows) => {
-            if(err == null) {
-                success(console.log(`${queries.content} added to table`));
-            } else {
-                failure(err);
-            }
-        });
-    },
+  //  Chat Table Queries
+  createChat(queries, success, failure = console.log) {
+    con.query(
+      "INSERT INTO chat SET user1_id = ? AND user2_id = ?",
+      [queries.user1, queries.user2],
+      (err, rows) => {
+        if (err == null) {
+          console.log("Chat added!");
+        } else {
+          failure(err);
+        }
+      }
+    );
+  },
 
-    //  Chat Table Queries
-    createChat(queries, success, failure = console.log) {
-        con.query('INSERT INTO chat SET user1_id = ? AND user2_id = ?', [queries.user1, queries.user2], (err, rows) => {
-            if(err == null) {
-                console.log('Chat added!');
-            } else {
-                failure(err);
-            }
-        });
-    },
-
-    getChatID(queries, success, failure = console.log) {
-        con.query('SELECT chat_id FROM chat WHERE (user1_id = ? AND user2_id = ?) OR (user2_id = ? AND user1_id = ?)', [queries.user1, queries.user2, queries.user1, queries.user2], (err, rows) => {
-            if(err == null) {
-                success(rows[0]);
-            } else {
-                failure(err);
-            }
-        });
-    },
-
+  getChatID(queries, success, failure = console.log) {
+    con.query(
+      "SELECT chat_id FROM chat WHERE (user1_id = ? AND user2_id = ?) OR (user2_id = ? AND user1_id = ?)",
+      [queries.user1, queries.user2, queries.user1, queries.user2],
+      (err, rows) => {
+        if (err == null) {
+          success(rows[0]);
+        } else {
+          failure(err);
+        }
+      }
+    );
+  },
 };
--- index.js
+++ index.js
@@ -1,117 +1,135 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.argv[2] || 4000;
-const path = require('path');
-const sha256 = require('js-sha256');
+const path = require("path");
+const sha256 = require("js-sha256");
 
-const session = require('express-session');
-app.enable('trust proxy');
+const session = require("express-session");
+app.enable("trust proxy");
 
-const dao = require('./dao.js');
-const { json } = require('body-parser');
-const { count } = require('console');
+const dao = require("./dao.js");
+const { json } = require("body-parser");
+const { count } = require("console");
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
 
 app.use((req, res, next) => {
-    console.log(`From ${req.ip}, Request ${req.url}`);
-    next();
+  console.log(`From ${req.ip}, Request ${req.url}`);
+  next();
 });
 
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
     saveUninitialized: true,
     proxy: true,
-}));
+  })
+);
 
 //  global variables
-const uid = 0;
+let uid = 0;
 const active = [];
 
 //  connection test
-app.get('/', (req, res) => {
-    console.log('Request ' + req.url + ' from ' + req.ip);
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello World!');
-    res.end();
+app.get("/", (req, res) => {
+  console.log("Request " + req.url + " from " + req.ip);
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello World!");
+  res.end();
 });
 
 //  produce client ID and add it to the table and return it
-app.get('/api/getClientId', (req, res) => {
-    let id = 'C' + uid;
-    uid += 1;
-    let user = {client_id: id};
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(user));
-    dao.getClientId(id, () => {});
-    res.end();
+app.get("/api/getClientId", (req, res) => {
+  let id = "C" + uid;
+  uid += 1;
+  let user = { client_id: id };
+  res.setHeader("Content-Type", "application/json");
+  res.write(JSON.stringify(user));
+  dao.getClientId(id, () => {});
+  res.end();
 });
 
 //  Get email and password --> if authentication succeeded returns name and id else return empty array
-app.get('/api/login/:email/:password', (req, res) => {
+app.get("/api/login/:email/:password", (req, res) => {
+  //   req.params.password = sha256(req.params.password);
+  try {
     req.params.password = sha256(req.params.password);
     dao.getProviderByEmail(req.params, (user) => {
-        res.setHeader('Content-Type', 'application/json');
-        //  user.length --> gives us the number of elements in array
-        res.write(JSON.stringify(user));
-        //  add the user in global active array
-        if(active.indexOf(user.provider_id) == -1){
-            active.push(user.provider_id);
-        }
-        res.end();
+      res.setHeader("Content-Type", "application/json");
+      //  user.length --> gives us the number of elements in array
+      res.write(JSON.stringify(user));
+      //  add the user in global active array
+      if (active.indexOf(user.provider_id) == -1) {
+        active.push(user.provider_id);
+      }
+      res.end();
     });
+  } catch (err) {
+    console.log(err);
+  }
+  //   req.params.password = sha256(req.params.password);
+  //   dao.getProviderByEmail(req.params, (user) => {
+  //     res.setHeader("Content-Type", "application/json");
+  //     //  user.length --> gives us the number of elements in array
+  //     res.write(JSON.stringify(user));
+  //     //  add the user in global active array
+  //     if (active.indexOf(user.provider_id) == -1) {
+  //       active.push(user.provider_id);
+  //     }
+  //     res.end();
+  //   });
 });
 
 //  remove the user from active array
-app.get('/api/logout/:id', (req, res) => {
-    const index = active.indexOf(req.params.id);
-    if (index > -1) {
-        active.splice(index, 1);
-        res.setHeader('Content-Type', 'text/plain');
-        res.send("you have logged out successfully");
-    } else {
-        console.log('error');
-        res.end();
-    }
+app.get("/api/logout/:id", (req, res) => {
+  const index = active.indexOf(req.params.id);
+  if (index > -1) {
+    active.splice(index, 1);
+    res.setHeader("Content-Type", "text/plain");
+    res.send("you have logged out successfully");
+  } else {
+    console.log("error");
+    res.end();
+  }
 });
 
 //  get full chat history
-app.get('/api/getConversationHistory/:sender/:receiver', (req, res) => {
-    dao.getConversationHistory(req.params, (chat) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(chat));
-        res.end();
-    });
+app.get("/api/getConversationHistory/:sender/:receiver", (req, res) => {
+  dao.getConversationHistory(req.params, (chat) => {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(chat));
+    res.end();
+  });
 });
 
 //  add to message table and the return the whole chat history with new message
-app.post('/api/addConversation', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    dao.postConversation(req.body, () => {
-        dao.getConversationHistory(req.body, (chat) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify(chat));
-            res.end();
-        });
+app.post("/api/addConversation", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  dao.postConversation(req.body, () => {
+    dao.getConversationHistory(req.body, (chat) => {
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(chat));
+      res.end();
     });
+  });
 });
 
 // Chat Table API --> Not Working right now
-app.get('/api/createChat/:user1/:user2', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    dao.createChat(req.params, (chat) => {});
-    res.end();
+app.get("/api/createChat/:user1/:user2", (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  dao.createChat(req.params, (chat) => {});
+  res.end();
 });
 
-const server = app.listen(port, function(){
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(`server listening to ${host}:${port}`);
+const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`server listening to ${host}:${port}`);
 });
